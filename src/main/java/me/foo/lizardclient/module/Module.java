d966280be6affc8bc9884111a0d99f1acd6a1875
unknown <amir@eggwithlegs.com>
2020-07-27 16:56:05 +1200

    initial commit

---
+++ src/main/java/me/foo/lizardclient/module/Module.java
@@ -0,0 +1,7 @@
+package me.foo.lizardclient.module;
+
+public class Module {
+
+	
+	
+}
---
+++ src/main/java/me/foo/lizardclient/module/ModuleManager.java
@@ -0,0 +1,10 @@
+package me.foo.lizardclient.module;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModuleManager {
+	
+	public List<Module> moduleList = new ArrayList<Module>();
+	
+}
